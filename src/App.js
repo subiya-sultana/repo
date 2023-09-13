38b928ffcc8438a522b18cb4cf07c6c4dfa7e680
subiya-sultana <itsmesubiya@gmail.com>
2023-09-13 06:22:12 +0530

    config react app for gh-pages deployment

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Education from './components/Education';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <About/>
+      <Education/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import homeBg from '../assets/homeBg.mp4'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight, faAngleDown } from '@fortawesome/free-solid-svg-icons'
+
+
+export default function Home() {
+  return (
+    <header className="App-header">
+        <video autoPlay muted loop src={homeBg} type="video/mp4"/>
+        <div className="overlay"></div>
+        <div className="home-content" >
+            <div className='home-left' data-aos="fade-down-right" data-aos-duration="2000"> 
+                <h1 >Hello, I am <span>Subiya Sultana.</span></h1>
+                <h1 data-aos="fade-left" data-aos-duration="2000">And I build Interactive websites.</h1>
+                <p>View My Work <FontAwesomeIcon className='icon' icon={faAngleDown} /></p>
+            </div>
+            <div className='home-right'>
+         
+              <button data-aos="flip-left" data-aos-duration="2000">Download Resume 
+                <FontAwesomeIcon className='icon' icon={faArrowRight} /></button>
+              <button data-aos="flip-left" data-aos-duration="2000">Explore About Me 
+                <FontAwesomeIcon className='icon' icon={faArrowRight} /></button>
+            
+            </div>
+        </div>
+        
+      </header>
+  )
+}
